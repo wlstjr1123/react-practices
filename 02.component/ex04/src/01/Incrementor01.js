519d653f11d2ed305f6c07f24c6b86fb0f746017
wlstjr1123 <wlstjr1123@naver.com>
2021-12-01 12:34:15 +0900

    state #finish

---
+++ 02.component/ex04/src/01/Incrementor01.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            step: this.props.step,
+            val: this.props.begin
+        }
+    }
+
+    onClickButton(e) {
+        this.setState({
+            val: this.state.val + this.props.step
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={(e) => {
+                    this.setState({
+                        val: this.state.val + this.state.step
+                    })
+                }}>
+                    <strong>+</strong>
+                </button>
+                {' '}
+                <span>{this.state.val}</span>
+            </div>
+        );
+    }
+}
