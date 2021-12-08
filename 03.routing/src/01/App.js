8469dc74384996d6f2893640e87e628c43d28dc9
wlstjr1123 <wlstjr1123@naver.com>
2021-12-08 10:39:50 +0900

    route #1

---
+++ 03.routing/src/01/App.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import Main from "./component/Main";
+import Gallery from "./component/Gallery";
+import Guestbook from "./component/Guestbook";
+
+export default function App() {
+    const [route, setRoute] = useState('');
+
+    useEffect(() => {
+        const handleHashChange = () => setRoute(window.location.hash.substr(1));
+
+        window.addEventListener('hashchange', handleHashChange);
+        return () => {
+            window.addEventListener('hashchange', handleHashChange);
+        }
+    }, [])
+
+    return (() => {
+        switch(route){
+            case '/':
+                return <Main />
+            case '/guestbook':
+                return <Guestbook />
+            case '/gallery':
+                return <Gallery />
+            default:
+                return null;
+        }
+    })();
+}
