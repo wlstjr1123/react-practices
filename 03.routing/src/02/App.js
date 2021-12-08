4b24f32e71af7bbaad6bd0bbc9d23d851b934e4e
wlstjr1123 <wlstjr1123@naver.com>
2021-12-08 11:21:13 +0900

    router#2

--- 03.routing/src/02/App.js
+++ 03.routing/src/02/App.js
@@ -6,10 +6,21 @@ import Guestbook from "./component/Guestbook";
 export default function App() {
     const [route, setRoute] = useState({page: '/'});
 
+    useEffect(() => {
+        const handlePopState = (e) => setRoute(e.state);
 
+        window.addEventListener('popstate', handlePopState);
+        return () => {
+            window.addEventListener('popstate', handlePopState);
+        }
+    }, [])
 
     const handleLinkClick = (e) => {
         e.preventDefault();
+
+        const url = e.target.href.substr(e.target.href.lastIndexOf('/'));
+        window.history.pushState({page: url}, e.target.text, url);
+        setRoute({page: url});
     }
 
     const router = function () {
@@ -31,6 +42,21 @@ export default function App() {
 
     return (
         <div>
+            {
+                (() => {
+                    switch(route.page){
+                        case '/':
+                            return <Main />
+                        case '/guestbook':
+                            return <Guestbook />
+                        case '/gallery':
+                            return <Gallery />
+                        default:
+                            return null;
+                    }
+                })()
+            }
+            <div>{route.page}</div>
             <ul>
                 <li><a href={'/'} onClick={handleLinkClick}>[Main]</a></li>
                 <li><a href={'/gallery'} onClick={handleLinkClick}>[Gallery]</a></li>
